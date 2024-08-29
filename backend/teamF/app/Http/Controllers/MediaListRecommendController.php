4f13a5f0441d013e5c8806d28ad58ca11b6a3ae5
i-yuki23 <yuki.ito.s7@dc.tohoku.ac.jp>
2024-08-29 09:13:14 +0900

    Contoroller名を変更

--- backend/teamF/app/Http/Controllers/MediaListRecommendController.php
+++ backend/teamF/app/Http/Controllers/MediaListRecommendController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Services\OpenAIService;
 use Illuminate\Http\Request;
 
-class OpenAIController extends Controller
+class MediaListRecommendController extends Controller
 {
     protected $openAIService;
     
@@ -23,6 +23,9 @@ class OpenAIController extends Controller
      */
     public function createMediaList(Request $request)
     {
+        // $mediaList = MediaList::fetchAll();     　　　　　　　　　　　　   ここでdbからメディアリストを取得する
+        // $press_release_info = $request->input('press_release_info');   press_releaseの情報をリクエストから取得
+
         var_dump($this->openAIService->generateRecomendedMediaList('media_list', 'press_release_info'));
         // return response()->json([
         //     'message' => 'success',
--- backend/teamF/routes/web.php
+++ backend/teamF/routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\OpenAIController;
+use App\Http\Controllers\MediaListRecommendController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/create', [OpenAIController::class, 'createMediaList'])->name('openai.createMediaList');
+Route::get('/create', [MediaListRecommendController::class, 'createMediaList'])->name('openai.createMediaList');
